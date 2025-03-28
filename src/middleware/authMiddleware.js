1336890982b55fe9de5c800e2a126833823b29fb
Michał Tarnowski <michal.tarnowski03@gmail.com>
2025-03-28 23:07:55 +0100

    authMiddleware created

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,43 @@
+const { verify } = require('jsonwebtoken');
+const { verifyToken } = require('../config/jwt');
+const User = require('../models/User');
+const { model } = require('mongoose');
+
+const authMiddleware = async (req, res, next) => {
+    const authHeader = req.header('Authorization');
+
+    if (!authHeader) {
+        return res.status(401).json({
+            message: 'No token, authorization denied',
+        });
+    }
+
+    const token = authHeader.replace('Bearer ', '');
+
+    try {
+        const decoded = verifyToken(token);
+
+        if (!decoded) {
+            return res.status(401).json({
+                message: 'Token is not valid'
+            });
+        }
+
+        const user = await User.findById(decoded.id).select('-password');
+
+        if (!user) {
+            return res.status(401).json({
+                message: 'User not found',
+            });
+        }
+
+        req.user = user;
+        next();
+    } catch (error) {
+        res.status(401).json({
+            error: `Token is not valid: ${error.message}`,
+        });
+    }
+}
+
+module.exports = authMiddleware;
