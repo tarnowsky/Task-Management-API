614184394adeb733bb4ac176297e9c34bb5b3571
Michał Tarnowski <michal.tarnowski03@gmail.com>
2025-03-28 22:43:21 +0100

    user model created

---
+++ src/models/Users.js
@@ -0,0 +1,45 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 3,
+        maxlength: 50,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    }
+}, {
+    timestamps: true // Automatically adds createdAt and updatedAt fields
+});
+
+UserSchema.pre('save', async (next) => {
+    if (!this.isModified('password')) return next();
+
+    try {
+        const salt = await bcrypt.genSalt();
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
+UserSchema.methods.isValidPassword = async (candidatePassword) => {
+    return bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
+
